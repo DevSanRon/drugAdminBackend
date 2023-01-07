17a65027b04e4e73fac1c75db63ac9ce294a0440
Ronald Thayil <thukprivateltd@gmail.com>
2023-01-08 02:23:43 +0530

    ..12

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const path = require("path");
-var fs = require('fs');
+var fs = require("fs");
 const http = require("http");
 const router = require("./src/routes/index");
-var bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload');
-require('dotenv').config()
-
-
+var bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const { responseData } = require("./src/helpers/response");
+const statusCode = require("./src/helpers/statuscode");
+require("dotenv").config();
 
 let port = process.env.PORT || "3002";
 app.set("port", port);
@@ -18,60 +18,62 @@ app.use(cors());
 
 app.use(
   fileUpload({
-      limits: {
-          fileSize: 50*1024*1024,// 50 MB
-      },
-      abortOnLimit: true,
+    limits: {
+      fileSize: 50 * 1024 * 1024, // 50 MB
+    },
+    abortOnLimit: true,
   })
 );
 
 //for CORS
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
   res.setHeader("Access-Control-Allow-Headers", "*");
   req.header("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-
-
-mongoose.connect(process.env.Databaseconnection,
-{
+mongoose.connect(process.env.Databaseconnection, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}
-);
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
-  
 
 app.use(express.json());
-
+app.get("/monty", (req, res) => {
+  return responseData({
+    res,
+    statusCode: statusCode.SUCCESS,
+    success: 1,
+    message: "Checkkkkkkk",
+  });
+  // res.send("API CALLING");
+});
 app.use("/api", router);
 app.use("/uploads", express.static("src/uploads"));
 
-
-
-
-const httpServer = http.createServer( app);
-console.log('API Server created in HTTPS mode');
+const httpServer = http.createServer(app);
+console.log("API Server created in HTTPS mode");
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 httpServer.listen(port);
-httpServer.on('error', onError);
-httpServer.on('listening', onListening);
-
+httpServer.on("error", onError);
+httpServer.on("listening", onListening);
 
 function onError(error) {
   if (error.syscall !== "listen") throw error;
@@ -101,4 +103,3 @@ function onListening() {
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
   console.log(`Listening on ${bind}`);
 }
-
